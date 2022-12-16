f990b6cb65afabe1dc37e1831c95a80aa1004f74
Pedro A. Aranda Gutiérrez <paaguti@gmail.com>
2022-12-16 14:05:00 +0100

    Textadept YAML module version 0.1
    Based on the deprecated YAML module, but in pure Lua.
    Depends on `yamllint`being installed in the PATH

---
+++ init.lua
@@ -0,0 +1,93 @@
+-- Copyright 2007-2022 Mitchell. See LICENSE.
+
+local M = {}
+
+--[[ This comment is for LuaDoc.
+---
+-- The YAML module for Textadept.
+-- It provides utilities for editing YAML documents.
+--
+-- ### Requirements
+-- Install yamllint in your PATH
+--
+-- ### Key Bindings
+--
+-- + `Ctrl+&` (`⌘&` | `M-&`)
+--   Jump to the anchor for the alias under the caret.
+module('_M.yaml')]]
+
+local lyaml = nil
+
+-- Returns the output of yamllint as a table of strings
+-- @param fname: the file to lint
+-- @return outputof yamllint split in lines
+
+local function yamllint(fname)
+  local result={}
+  local yamllint = assert(io.popen('yamllint -f parsable ' .. fname, 'r'))
+  local output = yamllint:read('*all')
+  yamllint:close()
+  for str in string.gmatch(output, "([^\n]+)") do
+      table.insert(result, str)
+    end
+  return result
+end
+
+-- Always use spaces.
+events.connect(events.LEXER_LOADED, function(name)
+  if name ~= 'yaml' then return end
+  buffer.use_tabs = false
+  buffer.word_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*'
+end)
+
+-- Commands.
+
+-- Verify syntax.
+events.connect(events.FILE_AFTER_SAVE, function()
+  if buffer:get_lexer() ~= 'yaml' or not lyaml then return end
+  buffer:annotation_clear_all()
+  local parse_result = yamllint(buffer.filename)
+  -- if ok then return end
+  for _, v in ipairs(parse_result) do
+    fname, line, col, cat, msg = v:match('^([^:]+):(%d+):(%d+): ([^ ]+) (.+)$')
+    if cat == '[error]' then
+      if not line or not col then line, col, msg = 1, 1, errmsg end
+      buffer.annotation_text[line] = msg
+      buffer.annotation_style[line] = buffer:style_of_name(lexer.ERROR)
+      buffer:goto_pos(buffer:find_column(line, col))
+    end
+  end
+end)
+
+---
+-- Jumps to the anchor for the alias underneath the caret.
+-- @name goto_anchor
+function M.goto_anchor()
+  local s = buffer:word_start_position(buffer.current_pos, true)
+  local e = buffer:word_end_position(buffer.current_pos)
+  local anchor = buffer:text_range(s, e):match('^%*(.+)$')
+  if anchor then
+    buffer:target_whole_document()
+    buffer.search_flags = buffer.FIND_WHOLEWORD
+    if buffer:search_in_target('&' .. anchor) ~= -1 then buffer:goto_pos(buffer.target_start) end
+  end
+end
+
+keys.yaml[CURSES and 'meta+&' or OSX and 'cmd+&' or 'ctrl+&'] = M.goto_anchor
+
+-- Initialise lyaml
+
+local function which(fname)
+  local path=os.getenv("PATH")
+  for dir in string.gmatch(path, "([^:]+)") do
+    local fn=dir..'/'..fname
+    local f=io.open(fn,"r")
+    if f~=nil then
+      io.close(f)
+      return fn
+    end
+  end
+end
+
+lyaml=which('yamllint')
+return M
